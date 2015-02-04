47fa57aa63026205f8e0ace3d2f479d69cce511f
Sam Brenner <samjbrenner@gmail.com>
2015-02-04 12:08:20 -0500

    image grid

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # js-cooperhewitt-images
-image size toggling assistance
+
+Image layout functionality as used on the [Cooper Hewitt Online Collections](http://collection.cooperhewitt.org). See `example.html` for examples.
+
+## Thumbnail Grids: `jquery.imageGrid.js`
+
+`jquery.imageGrid.js` creates a grid of square thumbnails with rollover functionality. It assumes your markup is structured thusly:
+
+```
+<ul id="myImageGrid" class="thumbnails">
+    <li class="thumbnail">
+    	<a href="#">
+	   <img class="out" src="/path/to/image.jpg" data-allow-redraw />
+	</a>
+    </li>
+</ul>
+```
+
+and is invoked by calling `imageGrid()` on the jQuery element like so: `$('#myImageGrid').imageGrid()`
+
+### Rollover
+It also assumes your images are named in a fashion where the rollover and rollout images for a single thumbnail share a common root with a stated size, for example `image1_out.jpg` and `image1_over.jpg`. You can configure these extensions and other parameters by using `data` attribues on the outer wrapper. Specifically:
+* `data-mouseout-image-size` The unique part of the mouseout image's filename. In `image1_out.jpg`, this is the string `out`. The default is `out`.
+* `data-mouseover-image-size` The unique part of the mouseover image's filename. In `image1_over.jpg`, this is the string `over`. The default is `over`
+* `data-mouseout-class-name` A class that will be applied to the mouseout image. Defaults to `out`.
+* `data-mouseover-class-name` A class that will be applied to the mouseout image. Defaults to `over`.
+
+### Image Alignment
+While the thumbnails are always square, they are capable of displaying non-square images. A class name of `portrait` or `landscape` will be applied accordingly. In the case of `landscape` images, a `top` CSS attribute will be calculated so as to vertically center the images. `portrait` images are centered in the CSS file.
+
+### Other notes:
+* Removing `data-allow-redraw` from within a `li.thumbnail` will disable the rollover code for an individual thumbnail.
+* The `a href` doesn't have to be `#` - make it whatever URL you want.
---
+++ jquery.imageGrid.js
@@ -0,0 +1,121 @@
+// jQuery Image Grid Plugin
+
+(function($) {
+	var _initThumbnailImageRollovers = function() {
+		var $thumbnailsHolder = $('.thumbnails');
+		var $thumbnails = $('.thumbnail');
+
+		if($thumbnails.length == 0) return;
+
+		var outSize = $thumbnailsHolder.data('mouseout-image-size') || 'out';
+		var outClass = $thumbnailsHolder.data('mouseout-class-name') || 'out';
+		var overSize = $thumbnailsHolder.data('mouseover-image-size') || 'over';
+		var overClass = $thumbnailsHolder.data('mouseover-class-name') || 'over';
+
+		//add rollover images to DOM through JS
+		$thumbnails.each(function() {
+			var $thumbnail = $(this);
+			var $thumbnailImg = $thumbnail.find('img');
+
+			if($thumbnailImg.data('load-error')) {
+				return;
+			}
+			
+			//modify DOM to add wrapper divs and classnames
+			$thumbnail.find('a').wrap('<div class="image-wrapper"></div>');
+			$thumbnailImg.addClass(outClass);
+
+			if(! $thumbnailImg.is('[data-allow-redraw]')){
+				return;
+			}
+			
+			//duplicate and change image source
+			var hoverImgSrc = $thumbnailImg.attr('src').replace(outSize,overSize);
+
+			var $thumbnailHoverImg = $thumbnailImg
+				.clone()
+				.removeClass(outClass)
+				.addClass(overClass + ' hidden')
+				.attr('src', '')
+				.insertAfter($thumbnailImg)
+				.attr('src', hoverImgSrc);
+
+			if($thumbnailImg.attr('id')) {
+				$thumbnailHoverImg.attr('id', $thumbnailImg.attr('id') + '-hover');
+			}
+			
+			//vertical centering
+			_verticallyCenterThumbnail($thumbnailImg, false);
+			_verticallyCenterThumbnail($thumbnailHoverImg, true);
+		});
+
+		//mouse interaction
+		$thumbnails.filter(':has(img[data-allow-redraw])').on('mouseover', function() {
+			$(this).find('.' + outClass).addClass('hidden');
+			$(this).find('.' + overClass).removeClass('hidden');
+		}).on('mouseout', function() {
+			$(this).find('.' + outClass).removeClass('hidden');
+			$(this).find('.' + overClass).addClass('hidden');
+		});
+	};
+
+	var _verticallyCenterThumbnail = function($thumbnail, hide) {
+		var src = $thumbnail.attr('src');
+
+		$thumbnail
+			.attr('src', '')
+			.on('load', function(e) {
+				e.preventDefault();
+
+				var $this = $(this);
+
+				$this.removeClass('hidden');
+				var naturalHeight = $this.naturalHeight();
+				var naturalWidth = $this.naturalWidth();
+
+				var height = $this.outerHeight();
+				var width = $this.outerWidth();
+				if(hide) $this.addClass('hidden');
+
+				if(naturalWidth > naturalHeight) {
+					$this.addClass('landscape');
+
+					var thumbnailHeight = $this.parent().outerHeight();
+
+					//vertical centering
+					$this.css('top', 100 * ((thumbnailHeight / 2 - height / 2) / thumbnailHeight) + '%'); 
+				} else {
+					$this.addClass('portrait');
+				}
+			})
+			.attr('src', src);
+	}
+
+	
+	$.fn.imageGrid = function() {
+		_initThumbnailImageRollovers();
+	};
+})(jQuery);
+
+
+// jQuery Natural Height/Width Plugin (not Cooper Hewitt code)
+// https://gist.github.com/johan/2209957
+
+// jQuery.naturalWidth / jQuery.naturalHeight plugin for (already-loaded) images
+// Triple-licensed: Public Domain, MIT and WTFPL license - share and enjoy!
+
+(function($) {
+	function img(url) {
+		var i = new Image;
+		i.src = url;
+		return i;
+	}
+
+	if ('naturalWidth' in (new Image)) {
+		$.fn.naturalWidth  = function() { return this[0].naturalWidth; };
+		$.fn.naturalHeight = function() { return this[0].naturalHeight; };
+		return;
+	}
+	$.fn.naturalWidth  = function() { return img(this.src).width; };
+	$.fn.naturalHeight = function() { return img(this.src).height; };
+})(jQuery);
